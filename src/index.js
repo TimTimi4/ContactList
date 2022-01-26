7eaa81b72575846e377b98570a8d5c96d9274bc4
TimTimi4 <doronin200894@gmail.com>
2022-01-26 18:13:22 +0600

    delete ie11 settings

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
-import 'react-app-polyfill/ie9'
-import 'react-app-polyfill/ie11'
-import 'react-app-polyfill/stable'
 
 ReactDOM.render(
   <React.StrictMode>
